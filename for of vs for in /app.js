160bd1ceec636151b7cd0f4557cacd1dc010d1f0
Muhammad Azhan <156644698+Azhandev@users.noreply.github.com>
2024-04-27 22:57:46 +0500

    Create app.js

---
+++ for of vs for in /app.js
@@ -0,0 +1,28 @@
+const fruits = ['banana', 'apple', 'peach', 'mango', 'grapes']
+
+
+
+
+// for(const letter of user) {
+//     console.log(letter);
+// }
+
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 50,
+    eyeColor: 'blue',
+    city: 'Bangalore',
+}
+
+// for(const key in person) {
+//     console.log(key, ': ' ,person[key]);
+// }
+
+const personKeys = Object.keys(person)
+const personValues = Object.values(person)
+const personEntries = Object.entries(person)
+// for (const key of personKeys) {
+//     console.log(person[key]);
+// }
+

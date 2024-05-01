d4ac149d3373e7ef7d93124897cdb28396e989b8
Muhammad Azhan <156644698+Azhandev@users.noreply.github.com>
2024-05-01 18:03:37 +0500

    Create script.js

---
+++ freeze object & seal object/script.js
@@ -0,0 +1,22 @@
+let username = 'Azhan'
+
+const user = {
+  firstName: 'Muhammad',
+  lastName: 'Azhan',
+  pata: {
+    city: 'Karachi',
+    pinCode: 813336,
+    state: 'USA',
+    moreDetails: {
+      population: 9798897897,
+      area: '787 sq km',
+    },
+  },
+  age: 19,
+  isGraduate: false,
+}
+
+// Object.seal(user)
+Object.freeze(user)
+
+console.log('isGraduate' in user);

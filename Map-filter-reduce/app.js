82628c25a26d72dfc0c7950c7664aac6c71dad73
Muhammad Azhan <156644698+Azhandev@users.noreply.github.com>
2024-05-11 22:24:06 +0500

    Update app.js

--- Map-filter-reduce/app.js
+++ Map-filter-reduce/app.js
@@ -1,5 +1,5 @@
 const months = ['January', 'February', 'March', 'April', 'May', 'December']
-
+ 
 // const capitalMonths = months.forEach((month, index) => {
 //     console.log(index + 1, month)
 //     return month.toUpperCase()
